943d18ee2776a744712866c7d09667db9173760b
DevSwist06 <151948612+DevSwist06@users.noreply.github.com>
2026-03-27 13:24:59 +0000

    1. Hello World!

---
+++ sample1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace sample1;
+
+public class Personne
+{
+    public string Nom { get; set; }
+    public int Age { get; set; }
+
+    public Personne(string nom, int age)
+    {
+        Nom = nom;
+        Age = age;
+    }
+
+    public string Hello(bool isLowercase)
+    {
+        string message = $"hello {Nom}, you are {Age}";
+        if (isLowercase)
+        {
+            return message.ToLower();
+        }
+        else
+        {
+            return message.ToUpper();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var personne = new Personne("Alice", 30);
+        Console.WriteLine(personne.Hello(true));
+        Console.WriteLine(personne.Hello(false));
+    }
+}
